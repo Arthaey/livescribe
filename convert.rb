cbb48e49a22cee1cf49016bac1673ba28d781c37
Arthaey <arthaey@gmail.com>
2014-09-13 20:21:46 -0700

    Fix whitespace around asterisks & em-dashes becoming hyphens.

--- convert.rb
+++ convert.rb
@@ -7,16 +7,26 @@ entities = HTMLEntities.new("html4")
 
 input = $stdin.read
 
-# TODO: write a custom Redcarpet renderer for Livescribe output?
-# http://dev.af83.com/2012/02/27/howto-extend-the-redcarpet2-markdown-lib.html
-
-# Livescribe-specific cleanup, to make it parsable as Markdown.
+# Livescribe exports things like apostrophes as decimal entities.
 input = entities.decode(input)
+
+# Livescribe breaks on every line manually.
 input.gsub!(/^<br>/, "")
-input.gsub!(/\.\s*$\n^([A-Z])/, ".\n\n\\1")
+
+# Livescribe doesn't preserve indented lines as new paragraphs.
+input.gsub!(/\.\s*$\n^([A-Z])/m, ".\n\n\\1")
+
+# Livescribe tends to surround asterisks with whitespace.
+input.gsub!(/\*\s*(.+?)\s*\*/m, "*\\1*")
+
+# Livescribe turns em-dashes into hyphens.
+input.gsub!(/(^|\s+)-(\s+|$)/, " — ")
 
 # Personal tweak: put smileys in <tt> tags.
 input.gsub!(/(\s|\b)+([:;][)(P])(\s|\b)*/, "\\1<tt>\\2</tt>\\3")
+
+# TODO: write a custom Redcarpet renderer for Livescribe output?
+# http://dev.af83.com/2012/02/27/howto-extend-the-redcarpet2-markdown-lib.html
 
 output = markdown.render(input)
 puts entities.decode(output)
