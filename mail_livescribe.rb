e42a4ed1ce3df913f606cf2e7bc580f43a059ef8
Arthaey <arthaey@gmail.com>
2014-10-21 23:11:38 -0700

    Added optional cc_email setting.

--- mail_livescribe.rb
+++ mail_livescribe.rb
@@ -5,11 +5,19 @@ require_relative "lib/settings.rb"
 input = $stdin.read
 output = Livescribe.to_html(input)
 
-Pony.mail(
-  :to        => Settings.to_email,
+mail_options = {
   :from      => Settings.from_email,
+  :to        => Settings.to_email,
+  :cc        => Settings.cc_email,
   :html_body => output,
   :via       => :sendmail
-)
+}
 
-puts "Email sent to #{Settings.to_email}."
+debug_msg = "Email sent to #{Settings.to_email}"
+if Settings["cc_email"]
+  mail_options[:cc] = Settings.cc_email
+  debug_msg += " and CC'd to #{Settings.cc_email}"
+end
+
+Pony.mail(mail_options)
+puts debug_msg
