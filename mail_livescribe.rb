a11cb25902c7a17f6a74567d0a8558e0bbef34dc
Arthaey <arthaey@gmail.com>
2015-03-24 09:55:51 -0700

    Log basic script information.

--- mail_livescribe.rb
+++ mail_livescribe.rb
@@ -1,7 +1,14 @@
+require "logger"
 require "mail"
 require "optparse"
 require_relative "lib/livescribe.rb"
 require_relative "lib/settings.rb"
+
+log_file = File.new(Settings.log, File::WRONLY | File::APPEND)
+logger = Logger.new(log_file)
+logger.level = Logger::DEBUG
+logger.info("=" * 50)
+logger.info("Starting mail_livescribe.rb script.")
 
 options = {}
 OptionParser.new do |opts|
@@ -18,15 +25,18 @@ OptionParser.new do |opts|
   opts.on("-c", "--[no-]cc EMAIL", "Cc: email address")       { |x| options[:cc]      = x }
   opts.on("-f", "--[no-]from EMAIL", "From: email address")   { |x| options[:from]    = x }
 end.parse!
+logger.info("Options: #{options.inspect}")
 
 input = $stdin.read
 if options[:email_input]
   input = Mail.new(input).body.decoded
 end
+logger.debug("Input:\n#{input}")
 
 livescribe = Livescribe.new(input, Settings.hashtag_deliveries)
 output = livescribe.to_html!
 puts output if options[:print]
+logger.debug("Output:\n#{output}")
 
 from_email = options[:from] || Settings.from_email
 to_email = options[:to] || livescribe.hashtag_delivery || Settings.to_email
@@ -48,9 +58,17 @@ if cc_email
   debug_msg += " and CC'd to #{cc_email}"
 end
 
-unless options[:dry_run]
+logger.debug("Email:\n#{mail.inspect}")
+if options[:dry_run]
+  debug_msg += " [DRY RUN]"
+else
   mail.delivery_method(:sendmail)
   mail.deliver
 end
 
 puts debug_msg if options[:verbose]
+logger.info(debug_msg)
+
+logger.info("Done")
+logger.info("=" * 40)
+logger.close
